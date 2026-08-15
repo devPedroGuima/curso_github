8438f3d997b0f14cb0b1e6c57095ee726accd6a6
devPedroGuima <pgfaciroliprotonmail.com>
2026-08-15 16:41:54 -0300

    adcionando novas funcionalidades ao cadastro

---
+++ cadstro.js
@@ -0,0 +1,9 @@
+function pegaemail (email){
+    return email
+
+    // pegando o email e cadastro na plataforma
+
+    function mostramensagem ()
+    returnno "Bem vindo ao sistema"
+
+    // Adcionando mensagem de boas vindas ao sistema
